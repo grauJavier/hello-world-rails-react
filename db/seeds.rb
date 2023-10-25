b34f5e9b42989a2c3775a8559db07b5ad4c18c64
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-24 23:06:47 -0300

    Indentation Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-Message.create(message: "Bonjour à tous !!") # French
-Message.create(message: "Buen día a todos!!") # Spanish
-Message.create(message: "Buongiorno a tutti!!") # Italian
-Message.create(message: "Goedemorgen allemaal!!") # Dutch
-Message.create(message: "Bom dia a todos!!") # Portuguese
-Message.create(message: "こんにちは皆さん！！") # Japanese
-Message.create(message: "صباح الخير للجميع!!") # Arabic
-Message.create(message: "Добро јутро свима!!") # Serbian
-Message.create(message: "God morgon allihopa!!") # Swedish
-Message.create(message: "Aloha a me ka laulā!!") # Hawaiian
-Message.create(message: "Hyvää huomenta kaikille!!") # Finnish
-Message.create(message: "God morgen alle sammen!!") # Norwegian
-Message.create(message: "Guten Morgen alle zusammen!!") # German
-Message.create(message: "Добро јутро свима!!") # Serbian (Cyrillic)
-Message.create(message: "Dzień dobry wszystkim!!") # Polish
-Message.create(message: "God morgen alle sammen!!") # Norwegian
-Message.create(message: "Καλημέρα σε όλους!!") # Greek
-Message.create(message: "안녕하세요 모두에게!!") # Korean
-Message.create(message: "בוקר טוב לכולם!!") # Hebrew
-Message.create(message: "Günaydın herkese!!") # Turkish
+Message.create(message: 'Bonjour à tous !!') # French
+Message.create(message: 'Buen día a todos!!') # Spanish
+Message.create(message: 'Buongiorno a tutti!!') # Italian
+Message.create(message: 'Goedemorgen allemaal!!') # Dutch
+Message.create(message: 'Bom dia a todos!!') # Portuguese
+Message.create(message: 'こんにちは皆さん！！') # Japanese
+Message.create(message: 'صباح الخير للجميع!!') # Arabic
+Message.create(message: 'Добро јутро свима!!') # Serbian
+Message.create(message: 'God morgon allihopa!!') # Swedish
+Message.create(message: 'Aloha a me ka laulā!!') # Hawaiian
+Message.create(message: 'Hyvää huomenta kaikille!!') # Finnish
+Message.create(message: 'God morgen alle sammen!!') # Norwegian
+Message.create(message: 'Guten Morgen alle zusammen!!') # German
+Message.create(message: 'Добро јутро свима!!') # Serbian (Cyrillic)
+Message.create(message: 'Dzień dobry wszystkim!!') # Polish
+Message.create(message: 'God morgen alle sammen!!') # Norwegian
+Message.create(message: 'Καλημέρα σε όλους!!') # Greek
+Message.create(message: '안녕하세요 모두에게!!') # Korean
+Message.create(message: 'בוקר טוב לכולם!!') # Hebrew
+Message.create(message: 'Günaydın herkese!!') # Turkish
